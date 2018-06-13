a2e27ba6a4c01d33ae033b7c422b8c71c1971098
CodingAndresPM <40184503+CodingAndresPM@users.noreply.github.com>
2018-06-13 14:45:08 -0700

    Add files via upload

---
+++ myJavaScript1.js
@@ -0,0 +1,7 @@
+function myJavaScript1() {
+  document.getElementById("boi").innerHTML = "jimmy neutron";
+}
+
+function myJavaScript3() {
+  document.getElementById("gorl").innerHTML = "be proud";
+}
