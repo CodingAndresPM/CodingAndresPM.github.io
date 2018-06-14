a77dce9624aec6ee3e9db2e92ce99c7d934bc8c7
CodingAndresPM <40184503+CodingAndresPM@users.noreply.github.com>
2018-06-14 11:43:17 -0700

    Add files via upload

--- myJavaScript1.js
+++ myJavaScript1.js
@@ -1,7 +1,13 @@
 function myJavaScript1() {
-  document.getElementById("boi").innerHTML = "jimmy neutron";
+  document.getElementById("boi").innerHTML = "3_phxgamers used to be three people that have both moved on from gaming and started to do their own thing";
 }
-
+  
 function myJavaScript3() {
-  document.getElementById("gorl").innerHTML = "be proud";
+  document.getElementById("gorl").innerHTML = "One of them went to Sound Cloud and the other one just gave up";
+}
+function myJavaScript5() {
+  document.getElementById("spagett").innerHTML = "you should really check him out on YouTube"}
+
+  function myJavaScript7() {
+  document.getElementById("garbage").innerHTML = "BOOM! This button is one of my newest skills I have learned in Coding in Colors!";
 }
